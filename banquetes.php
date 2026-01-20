521145389bb72e82416d245550bbca33ddf55cdc
bcrodas <bcrodas@espe.edu.ec>
2026-01-20 11:42:40 -0500

    Ejercicio 2

--- banquetes.php
+++ banquetes.php
@@ -4,6 +4,7 @@
 </head>
 <body>
     <?php
+    /*ejercicio Bárbara Rodas*/
     /*“La langosta ahumada” es una empresa dedicada a ofrecer banquetes; sus tarifas son las siguientes: 
         el costo de platillo por persona es de $95.00, pero si el número de personas es mayor a 200 pero 
         menor o igual a 300, el costo es de $85.00. Para más de 300 personas el costo por platillo es de $75.00. 
